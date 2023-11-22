f488f564c71d865703ea7031160bd9b0ca225c07
Eugene Walker <121591687+WalkerVision@users.noreply.github.com>
2023-11-22 18:28:15 -0500

    test

---
+++ Guessing Game/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+        static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the guessing game.");
+        Console.WriteLine("Pick a number from 1 to 10");
+        String userchoice = Console.ReadLine();
+      
+
+    }
+}
