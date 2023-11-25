a79e080a094007c26b69587b86a6f48a60b18bb6
Eugene Walker <walker_vision@icloud.com>
2023-11-25 16:45:06 -0500

    added code

--- Guessing Game/Program.cs
+++ Guessing Game/Program.cs
@@ -2,12 +2,16 @@
 
 class Program
 {
-        static void Main(string[] args)
+    static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the guessing game.");
-        Console.WriteLine("Pick a number from 1 to 10");
+        Console.WriteLine("Pick a number from 1 to 3");
         String userchoice = Console.ReadLine();
-      
 
+        while (userchoice != "1" && userchoice != "2" && userchoice != "3")
+        {
+            Console.WriteLine("Invaild choice please eenter 1,2,or 3");
+            userchoice = Console.ReadLine();
+        }
     }
 }
